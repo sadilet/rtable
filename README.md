3793278828d33270197dce3ff73e12cb97804523
Denis <candyboobers@gmail.com>
2024-02-16 23:50:49 +0100

    Show data slice in a simple html table

---
+++ README.md
@@ -0,0 +1,4 @@
+### To get started development
+- cargo install wasm-pack
+- Add $HOME/.cargo/bin to $PATH
+- wasm-pack build --target web
--- index.js
+++ index.js
@@ -6,7 +6,28 @@ const runWasm = async () => {
     const tt = new Table();
     console.log(tt.rows(0, 25))
     globalThis.tt = tt;
-    // Set the result onto the body
-    document.body.textContent = `Hello World! addResult: ${tt.len()}`;
+
+    globalThis.tt.rows(0, 2500).forEach(it => {
+        const tr = document.createElement('tr');
+
+        let td = document.createElement('td')
+        td.appendChild(document.createTextNode(it.Mercury))
+        tr.appendChild(td);
+
+        td = document.createElement('td')
+        td.appendChild(document.createTextNode(it.Venus))
+        tr.appendChild(td);
+
+        td = document.createElement('td')
+        td.appendChild(document.createTextNode(it.Mars))
+        tr.appendChild(td);
+
+        td = document.createElement('td')
+        td.appendChild(document.createTextNode(it.Earth))
+        tr.appendChild(td);
+
+        document.getElementById('main-table').appendChild(tr);
+    });
 };
-runWasm();
+
+await runWasm();
